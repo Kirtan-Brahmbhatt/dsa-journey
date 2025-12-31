3abe94b64dba3269bd7114fe712fe914466aca46
Kirtan Brahmbhatt <kirtanbarot0@gmail.com>
2025-12-31 22:53:12 +0530

    Immproved code for arraay Min-Max

--- Arrays/min-max.py
+++ Arrays/min-max.py
@@ -1,3 +1,28 @@
-a=10
-b=20
-print(a+b)
+class MinMaxArray:
+    def __init__(self, arr):
+        self.arr = arr
+
+    def find_min_max(self):
+        min_val = self.arr[0]
+        max_val = self.arr[0]
+
+        for i in range(1, len(self.arr)):
+            if self.arr[i] < min_val:
+                min_val = self.arr[i]
+            if self.arr[i] > max_val:
+                max_val = self.arr[i]
+
+        print("Minimum:", min_val)
+        print("Maximum:", max_val)
+
+
+# Taking input from user
+n = int(input("Enter number of elements: "))
+arr = []
+
+for i in range(n):
+    val = int(input("Enter element: "))
+    arr.append(val)
+
+obj = MinMaxArray(arr)
+obj.find_min_max()
